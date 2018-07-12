08bb606724454be53f9dd77ab1279c4389b5a221
Caleb Baird <caleb@bicodetech.com>
2018-07-12 16:03:13 -0600

    Get rid of php!!!!

--- node/family-chore-board/views/partials/header.php
+++
@@ -1,7 +0,0 @@
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width" />
-	<link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
-	<script src="js/jquery-3.3.1.min.js"></script>
-	<script src="js/main.js" charset="utf-8"> </script>
-</head>
-<body>
